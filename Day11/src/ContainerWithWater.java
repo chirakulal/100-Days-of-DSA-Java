12e1ad58e5dfd2a5259ac2eb65ee531100fde97b
chirakulal <chirashreelk@gmail.com>
2026-02-02 22:40:04 +0530

    "Array"

---
+++ Day11/src/ContainerWithWater.java
@@ -0,0 +1,30 @@
+public class ContainerWithWater {
+
+    public static int maxArea(int[] height){
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            int width = right - left;
+            int minHeight = Math.min(height[left], height[right]);
+            int area = width * minHeight;
+
+            maxArea = Math.max(maxArea, area);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+
+    public static void main(String[] args) {
+        int[] height = {1, 5, 4, 3};
+
+        System.out.println(maxArea(height));
+    }
+}
